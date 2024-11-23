a856f89e0b7fe18ed19bb961037f4b60c00d6dcb
ŁotaŔ <155155069+Lotar473@users.noreply.github.com>
2024-11-23 17:40:09 +0900

    Crossbow emoji changed.

--- src/main/java/org/hoshizora/killadvertising/Listener.kt
+++ src/main/java/org/hoshizora/killadvertising/Listener.kt
@@ -1,4 +1,39 @@
 package org.hoshizora.killadvertising
 
-class Listener {
+import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.plugin.java.JavaPlugin
+
+class Listener(private val plugin: JavaPlugin) : Listener {
+
+    @EventHandler
+    fun onPlayerDeath(event: PlayerDeathEvent) {
+        val victim = event.entity
+        val killer = victim.killer
+
+        if (killer != null) {
+            val weapon = killer.inventory.itemInMainHand
+            val weaponName = if (weapon.type == Material.AIR) {
+                "Punch"
+            } else if (weapon.hasItemMeta() && weapon.itemMeta!!.hasDisplayName()) {
+                weapon.itemMeta!!.displayName
+            } else {
+                weapon.type.name.replace("_", " ")
+                    .lowercase()
+                    .split(" ")
+                    .joinToString(" ") { it.replaceFirstChar { char -> char.uppercaseChar() } }
+            }
+
+            val weaponEmoji = when (weapon.type) {
+                Material.BOW -> "➴"
+                Material.CROSSBOW -> "➴"
+                else -> "🗡"
+            }
+
+            event.deathMessage = "${killer.name}${ChatColor.YELLOW} [${ChatColor.RESET}${weaponName}${ChatColor.RESET}${ChatColor.YELLOW}]$weaponEmoji ${ChatColor.RED}${victim.name}"
+        }
+    }
 }
--- src/main/java/org/hoshizora/killadvertising/Main.kt
+++ src/main/java/org/hoshizora/killadvertising/Main.kt
@@ -4,10 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
-        // Plugin startup logic
+        logger.info("星空れなる｜HoshizoraRenaru EternaL Plugin")
+        logger.info("KillAd has been activated.")
+        server.pluginManager.registerEvents(Listener(this), this)
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("KillAd has been disabled.")
     }
 }
